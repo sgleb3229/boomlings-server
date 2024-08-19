c3548aa6ac855a065c0ef494a68a2183a5da0201
sgleb3229 <95977826+sgleb3229@users.noreply.github.com>
2024-08-19 13:10:15 +0500

    hype++

--- get_relative.php
+++ get_relative.php
@@ -6,13 +6,16 @@ if(isset($_POST['name'])){$name = $_POST['name'];}else if(isset($_GET['name'])){
 if(isset($_POST['score'])){$score = $_POST['score'];}else if(isset($_GET['score'])){$score = $_GET['score'];}else{echo -1; exit(-1);}
 if(isset($_POST['context'])){$context = $_POST['context'];}else if(isset($_GET['context'])){$context = $_GET['context'];}else{echo -1; exit(-1);}
 include "db.php";
-$usercheck = mysqli_fetch_array(mysqli_query($conn, "SELECT count(*),score FROM users WHERE udid='$udid'"));
+$usercheck = mysqli_fetch_array(mysqli_query($conn, "SELECT count(*),score,context FROM users WHERE udid='$udid'"));
 if($usercheck["count(*)"] == 0){
 mysqli_query($conn, "INSERT INTO users (udid,score,context,name) VALUES ('$udid',$score,$context,'$name')");
 }else{
 $dbscore = $usercheck["score"];
+$dbcontext = $usercheck["context"];
 if($dbscore <= $score){
 mysqli_query($conn, "UPDATE users SET score=$score, context=$context, name='$name' WHERE udid='$udid'");
+}else if($dbcontext <= $context){
+mysqli_query($conn, "UPDATE users SET context=$context, name='$name' WHERE udid='$udid'");
 }else{
 mysqli_query($conn, "UPDATE users SET name='$name' WHERE udid='$udid'");
 }
